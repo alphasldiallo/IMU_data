29b570c2f74ca331568c91d1615d00c3137e9091
Alpha Diallo <alphadiallo009@gmail.com>
2020-12-29 16:01:46 +0100

    Sockets

---
+++ sockets/client.py
@@ -0,0 +1,43 @@
+import socket, sys
+class MySocket:
+    """demonstration class only
+      - coded for clarity, not efficiency
+    """
+
+    def __init__(self, sock=None):
+        if sock is None:
+            self.sock = socket.socket(
+                            socket.AF_INET, socket.SOCK_STREAM)
+        else:
+            self.sock = sock
+
+    def connect(self, host, port):
+        self.sock.connect((host, port))
+        print("Connection made on {}:{}".format(host, port))
+
+    def mysend(self, msg):
+        totalsent = 0
+        while totalsent < len(msg):
+            sent = self.sock.send(bytes(str(msg[totalsent:]), encoding="utf8"))
+            print("Message sent: {}".format(msg[totalsent:]))
+            if sent == 0:
+                raise RuntimeError("socket connection broken")
+            totalsent = totalsent + sent
+
+    def myreceive(self):
+        chunks = []
+        bytes_recd = 0
+        MSGLEN = len(msg)
+        while bytes_recd < MSGLEN:
+            chunk = self.sock.recv(min(MSGLEN - bytes_recd, 2048))
+            if chunk == b'':
+                raise RuntimeError("socket connection broken")
+            chunks.append(chunk)
+            bytes_recd = bytes_recd + len(chunk)
+        return b''.join(chunks)
+
+
+if __name__ == '__main__':
+    s=MySocket()
+    s.connect(host="127.0.0.1", port=1223)
+    s.mysend([i for i in range(100)])
